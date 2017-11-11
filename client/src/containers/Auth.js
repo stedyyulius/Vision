b26c282c946858b4b2be61e7c33d9d8b39e5ba16
stedyyulius <stedyyulius@gmail.com>
2017-11-11 16:10:15 +0700

    api

--- client/src/containers/Auth.js
+++ client/src/containers/Auth.js
@@ -23,7 +23,7 @@ class Auth extends Component{
       <div className="body">
         <div className="login-form">
             <img className="icon" src="http://cliparting.com/wp-content/uploads/2016/06/Eye-clip-art-black-and-white-free-clipart-images.png" />
-            <p className="title">Meetup and Hackhaton Searher App</p>
+            <p className="title">Events Searher App</p>
         <div className="title">
           <Facebook />
         </div>
