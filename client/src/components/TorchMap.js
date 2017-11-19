44a25f3081b72fe85d98ff9a875c154e98abf4fb
stedyyulius <stedyyulius@gmail.com>
2017-11-19 21:39:23 +0700

    one for all endpoint for vr

--- client/src/components/TorchMap.js
+++ client/src/components/TorchMap.js
@@ -28,7 +28,7 @@ var Me = icon({
 });
 
 const startLocation = [37.483999 + 0.0003, -122.147233 - 0.0004]
-const vr = `http://localhost:8081/vr`
+const vr = `https://sarielab.github.io`
 
 class TorchMap extends Component {
   constructor(props){
